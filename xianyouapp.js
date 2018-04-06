b100409710bf340d9883c10976ebf1c61840a62e
caryangBingo <16424723+caryangBingo@users.noreply.github.com>
2018-04-06 22:53:55 +0800

    Update xianyouapp.js

--- xianyouapp.js
+++ xianyouapp.js
@@ -1,5 +1,5 @@
 var regex = /https:\/\/[^\s]*/g
 var match = regex.exec($context.link || $clipboard.link)
-var appurl = match[1]
+var appurl = match[0]
 $clipboard.text=appurl
 $app.openURL("xianyou://")
